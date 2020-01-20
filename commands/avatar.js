34ee48c5819a0c1f88dd6baa6eb9a317dab2d233
Ryan Jonathan <ryan@kyou.id>
2020-01-20 23:49:33 +0700

    Displaying avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,11 +1,21 @@
+const Discord = require("discord.js");
+
 module.exports = {
-  name: 'avatar',
-  description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
+  name: "avatar",
+  description: "Get the avatar URL of the tagged user(s), or your own avatar.",
   execute(message) {
+    console.log(message.author);
+    const profileEmbed = new Discord.RichEmbed()
+      .setTitle("Avatar Info")
+      .setColor("#0099ff")
+      .setDescription(
+        `${message.author.username}#${message.author.discriminator}`
+      )
+      .setURL(message.author.displayAvatarURL)
+      .setImage(message.author.displayAvatarURL);
+
     if (!message.mentions.users.size) {
-      return message.channel.send(
-        `Your avatar: ${message.author.displayAvatarURL}`
-      );
+      return message.channel.send(profileEmbed);
     }
 
     const avatarList = message.mentions.users.map(user => {
--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 const commandFiles = fs
-  .readdirSync('./commands')
-  .filter(file => file.endsWith('.js'));
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-  console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('message', message => {
+client.on("message", message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
@@ -30,9 +30,7 @@ client.on('message', message => {
     client.commands.get(command).execute(message, args);
   } catch (error) {
     console.error(error);
-    message.reply(
-      'there was an error trying to execute that command!'
-    );
+    message.reply("there was an error trying to execute that command!");
   }
 });
 
