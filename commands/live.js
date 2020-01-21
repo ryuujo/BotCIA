9f476c2ce87381bedf3513345653cda62a2f1aaf
unknown <>
2020-01-21 16:57:17 +0700

    fetch youtube info

--- commands/live.js
+++ commands/live.js
@@ -1,51 +1,54 @@
 const { RichEmbed } = require('discord.js');
 const moment = require('moment');
 const vliver = require('../constants/vliver');
+const fetchYoutube = require('youtube-info');
 
 module.exports = {
   name: 'live',
   description: 'Announces Upcoming live immediately',
   args: true,
-  execute(message, args) {
+  async execute(message, args) {
     moment.locale('id');
     if (args.length !== 4) {
       message.reply(
-        'Please type these arguments: `Vliver First Name`, `Livestream Date (DD/MM/YYYY)`, `Livestream Time (HH:MM)`, `Youtube Link (https://)`'
+        'Please type these arguments: `Vliver First Name`, `Livestream Date (DD/MM/YYYY)`, `Livestream Time (HH:MM)`, `Video ID (https://www.youtube.com/watch?v={.....})`'
       );
       return setTimeout(() => message.channel.bulkDelete(2), 5000);
     }
 
-    const dateSplit = args[1].split('/');
-    const date = dateSplit[1] + '/' + dateSplit[0] + '/' + dateSplit[2];
+    try {
+      const dateSplit = args[1].split('/');
+      const date = dateSplit[1] + '/' + dateSplit[0] + '/' + dateSplit[2];
+      const vliverFirstName = args[0].toLowerCase();
+      const livestreamDateTime = moment(
+        Date.parse(`${date} ${args[2]}`)
+      ).format('Do MMMM YYYY, HH:mm');
+      const youtubeId = args[3];
+      const vData = vliver[vliverFirstName];
 
-    message.channel.bulkDelete(1).catch(err => {
+      const youtubeData = await fetchYoutube(youtubeId);
+
+      await message.channel.bulkDelete(1);
+      const liveEmbed = new RichEmbed()
+        .setColor(vData.color)
+        .setAuthor(vData.fullName, vData.avatarURL, vData.channelURL)
+        .setTitle(`${vData.fullName} akan melakukan live!`)
+        .setURL('https://youtube.com/watch?v=' + youtubeId)
+        .setDescription(
+          `Stream akan dimulai ${moment(
+            livestreamDateTime,
+            'Do MMMM YYYY, HH:mm'
+          ).fromNow()}`
+        )
+        .setThumbnail(vData.avatarURL)
+        .addField('Tanggal & Waktu Livestream', livestreamDateTime, true)
+        .addField('Link Video Youtube', youtubeData.url, true)
+        .addField('Judul Livestream', youtubeData.title)
+        .setImage(youtubeData.thumbnailUrl);
+      await message.channel.send(liveEmbed);
+    } catch (err) {
       console.error(err);
-      return message.channel.send('I need permission to delete your message');
-    });
-
-    const vliverFirstName = args[0].toLowerCase();
-    const livestreamDateTime = moment(Date.parse(`${date} ${args[2]}`)).format(
-      'Do MMMM YYYY, HH:mm'
-    );
-    const youtubeLink = args[3];
-
-    const vData = vliver[vliverFirstName];
-
-    const liveEmbed = new RichEmbed()
-      .setColor(vData.color)
-      .setAuthor(vData.fullName, vData.avatarURL, vData.channelURL)
-      .setTitle(`${vData.fullName} akan melakukan live!`)
-      .setURL(youtubeLink)
-      .setDescription(
-        `Stream akan dimulai ${moment(
-          livestreamDateTime,
-          'Do MMMM YYYY, HH:mm'
-        ).fromNow()}`
-      )
-      .setThumbnail(vData.avatarURL)
-      .addField('Tanggal & Waktu Livestream', livestreamDateTime, true)
-      .addField('Link Video Youtube', youtubeLink, true);
-
-    message.channel.send(liveEmbed);
+      return message.channel.send('Something error in here: ', err.message);
+    }
   }
 };
