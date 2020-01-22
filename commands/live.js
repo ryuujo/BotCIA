85ebed53f3f07d5eb783acb93ef6266a211d361c
Ryan Jonathan <ryan@kyou.id>
2020-01-22 09:37:38 +0700

    Error Handling

--- commands/live.js
+++ commands/live.js
@@ -1,61 +1,67 @@
-const { RichEmbed } = require('discord.js');
-const moment = require('moment');
-const vliver = require('../constants/vliver');
-const fetchYoutube = require('youtube-info');
+const { RichEmbed } = require("discord.js");
+const moment = require("moment");
+const vliver = require("../constants/vliver");
+const fetchYoutube = require("youtube-info");
 
 module.exports = {
-  name: 'live',
-  description: 'Announces Upcoming live immediately',
+  name: "live",
+  description: "Announces Upcoming live immediately",
   args: true,
   async execute(message, args) {
-    moment.locale('id');
+    moment.locale("id");
     if (args.length !== 4) {
       message.reply(
-        'Tulis formatnya seperti ini ya:\n```!live [Nama depan vliver] [Tanggal Livestream (DD/MM)] [Waktu Livestream (HH:MM)] [Video ID (https://www.youtube.com/watch?v={.....})]```'
+        "Tulis formatnya seperti ini ya:\n```!live [Nama depan vliver] [Tanggal Livestream (DD/MM)] [Waktu Livestream (HH:MM)] [Video ID (https://www.youtube.com/watch?v={.....})]```"
       );
       return setTimeout(() => message.channel.bulkDelete(2), 5000);
     }
 
     try {
       await message.channel.startTyping();
-      const dateSplit = args[1].split('/');
+      const dateSplit = args[1].split("/");
       const date =
-        dateSplit[1] + '/' + dateSplit[0] + '/' + moment().format('YYYY');
+        dateSplit[1] + "/" + dateSplit[0] + "/" + moment().format("YYYY");
       const livestreamDateTime = moment(
         Date.parse(`${date} ${args[2]}`)
-      ).format('Do MMMM YYYY, HH:mm');
+      ).format("Do MMMM YYYY, HH:mm");
       const vliverFirstName = args[0].toLowerCase();
       const vData = vliver[vliverFirstName];
       const youtubeId = args[3];
-
-      const youtubeData = await fetchYoutube(youtubeId);
-
-      await message.channel.bulkDelete(1);
-      const liveEmbed = new RichEmbed()
-        .setColor(vData.color)
-        .setAuthor(vData.fullName, vData.avatarURL, vData.channelURL)
-        .setTitle(`${vData.fullName} akan melakukan Livestream!`)
-        .setURL('https://youtube.com/watch?v=' + youtubeId)
-        .setDescription(
-          `Stream akan dimulai ${moment(
-            livestreamDateTime,
-            'Do MMMM YYYY, HH:mm'
-          ).fromNow()}`
-        )
-        .setThumbnail(vData.avatarURL)
-        .addField('Tanggal & Waktu Livestream', livestreamDateTime, true)
-        .addField('Link Video Youtube', youtubeData.url, true)
-        .addField('Judul Livestream', youtubeData.title)
-        .setImage(youtubeData.thumbnailUrl);
-      await message.channel.stopTyping();
-      await message.channel.send(liveEmbed);
+      try {
+        const youtubeData = await fetchYoutube(youtubeId);
+        await message.channel.bulkDelete(1);
+        const liveEmbed = new RichEmbed()
+          .setColor(vData.color)
+          .setAuthor(vData.fullName, vData.avatarURL, vData.channelURL)
+          .setTitle(`${vData.fullName} akan melakukan Livestream!`)
+          .setURL("https://youtube.com/watch?v=" + youtubeId)
+          .setDescription(
+            `Stream akan dimulai ${moment(
+              livestreamDateTime,
+              "Do MMMM YYYY, HH:mm"
+            ).fromNow()}`
+          )
+          .setThumbnail(vData.avatarURL)
+          .addField("Tanggal & Waktu Livestream", livestreamDateTime, true)
+          .addField("Link Video Youtube", youtubeData.url, true)
+          .addField("Judul Livestream", youtubeData.title)
+          .setImage(youtubeData.thumbnailUrl);
+        await message.channel.stopTyping();
+        await message.channel.send(liveEmbed);
+      } catch (err) {
+        console.log(err);
+        message.channel.stopTyping();
+        message.channel.send(
+          `Ada sesuatu yang salah, tapi itu bukan kamu: ${err.message}`
+        );
+        return setTimeout(() => message.channel.bulkDelete(2), 5000);
+      }
     } catch (err) {
-      console.error(err);
       await message.channel.stopTyping();
-      return message.channel.send(
-        'Ada sesuatu yang salah, tapi itu bukan kamu: ',
-        err.message
+      message.channel.send(
+        `Ada sesuatu yang salah, tapi itu bukan kamu: ${err.message}`
       );
+      return setTimeout(() => message.channel.bulkDelete(2), 5000);
     }
   }
 };
