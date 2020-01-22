9796e83fd1772c51248160bb7f245e3847e59511
Ryuujo <mirukusheki@gmail.com>
2020-01-22 11:33:53 +0700

    authorization 3.0

--- main.js
+++ main.js
@@ -21,10 +21,12 @@ client.once('ready', () => {
 
 client.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (!client.commands.has(command)) return;
+
   if (message.member.roles.some(r => roles.includes(r.name))) {
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    if (!client.commands.has(command)) return;
     try {
       client.commands.get(command).execute(message, args);
     } catch (error) {
