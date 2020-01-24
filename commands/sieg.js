05efebab19bb82e4c9a355699d653a583a23e82f
Ryuujo <mirukusheki@gmail.com>
2020-01-24 12:51:30 +0700

    sieg heil

---
+++ commands/sieg.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'sieg',
+  description: 'HEIL!',
+  execute(message) {
+    message.channel.send("HEIL ( '-')/", {
+      file:
+        'https://cdn.discordapp.com/attachments/668781485427392543/670142770551193620/tenor.gif'
+    });
+  }
+};
