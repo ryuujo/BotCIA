d1f97a976cf8c3f2c102425c246a3937fcb076f8
Ryan Jonathan <ryan@kyou.id>
2020-02-12 21:13:05 +0700

    schedule updated to 30 minutes before

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,51 +1,53 @@
-const moment = require('moment');
-const { Op } = require('sequelize');
-const Schedule = require('../models').Schedule;
-const { name, version } = require('../package.json');
+const moment = require("moment");
+const { Op } = require("sequelize");
+const Schedule = require("../models").Schedule;
+const { name, version } = require("../package.json");
 
 module.exports = {
-  name: 'schedule',
-  description: 'Check upcoming schedule',
+  name: "schedule",
+  description: "Check upcoming schedule",
   async execute(message) {
-    moment.locale('id');
-    const timeFormat = 'Do MMMM YYYY, HH:mm';
+    moment.locale("id");
+    const timeFormat = "Do MMMM YYYY, HH:mm";
     try {
       const data = await Schedule.findAll({
-        where: { dateTime: { [Op.gt]: new Date() } },
+        where: {
+          dateTime: { [Op.gt]: new Date().setMinutes(new Date().getMinutes() - 30) }
+        },
         order: [
-          ['dateTime', 'ASC'],
-          ['id', 'ASC']
+          ["dateTime", "ASC"],
+          ["id", "ASC"]
         ],
         raw: true,
-        include: 'vliver'
+        include: "vliver"
       });
       const liveEmbed = {
-        color: parseInt(data.length !== 0 ? data[0]['vliver.color'] : ''),
-        title: 'Upcoming Stream',
+        color: parseInt(data.length !== 0 ? data[0]["vliver.color"] : ""),
+        title: "Upcoming Stream",
         description:
           data.length !== 0
             ? `${data
                 .map(
                   (d, i) =>
-                    `${i + 1}. __**${d['vliver.fullName']}**__ (${
+                    `${i + 1}. __**${d["vliver.fullName"]}**__ (${
                       d.type
                     })\nJudul Stream:**${
                       d.title
                     }**\nTanggal dan Waktu: **${moment(d.dateTime)
-                      .utcOffset('+07:00')
+                      .utcOffset("+07:00")
                       .format(timeFormat)} WIB / GMT+7** (*${moment(
                       d.dateTime
                     ).fromNow()}*)\n${d.youtubeUrl}\n\n`
                 )
-                .join('')}`
-            : '*Belum ada jadwal livestream untuk saat ini.*',
+                .join("")}`
+            : "*Belum ada jadwal livestream untuk saat ini.*",
         footer: {
           text: `${name} v${version} - This message was created on ${moment()
-            .utcOffset('+07:00')
+            .utcOffset("+07:00")
             .format(timeFormat)}`
         }
       };
-      return message.channel.send('List Stream/Premiere yang akan datang: ', {
+      return message.channel.send("List Stream/Premiere yang akan datang: ", {
         embed: liveEmbed
       });
     } catch (e) {
