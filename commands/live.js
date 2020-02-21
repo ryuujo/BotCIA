d141e998574406d5240fb5d1ae3fc311c22c3e9c
Ryuujo <mirukusheki@gmail.com>
2020-02-21 12:01:42 +0700

    Cleaning code

--- commands/live.js
+++ commands/live.js
@@ -16,123 +16,122 @@ module.exports = {
       prefix +
       "live [Nama depan vliver] [Tanggal Livestream (DD/MM)] [Waktu Livestream dalam WIB / GMT+7 (HH:MM)] [Link Video Youtube]```";
 
-    if (message.member.roles.some(r => roles.live.includes(r.name))) {
-      if (args.length !== 4) {
+    if (!message.member.roles.some(r => roles.live.includes(r.name))) {
+      return message.reply("", { file: "https://i.imgur.com/4YNSGmG.jpg" });
+    }
+    if (args.length !== 4) {
+      return message.reply(messages);
+    }
+    message.channel.send(
+      "Mohon tunggu, sedang menyiapkan data untuk dikirimkan"
+    );
+    const timeFormat = "Do MMMM YYYY, HH:mm";
+    const dateSplit = args[1].split("/");
+    const date =
+      dateSplit[1] + "/" + dateSplit[0] + "/" + moment().format("YYYY");
+    const dateTime = Date.parse(`${date} ${args[2]}`);
+    const livestreamDateTime = moment(dateTime)
+      .utcOffset("+07:00")
+      .format(timeFormat);
+    const livestreamDateTimeJapan = moment(dateTime)
+      .utcOffset("+09:00")
+      .format(timeFormat);
+    const vliverFirstName = args[0].toLowerCase();
+    const linkData = args[3].split("/");
+    let youtubeId;
+    if (linkData[0] !== "https:" || linkData[3] === "") {
+      return message.reply(messages);
+    }
+    switch (linkData[2]) {
+      case "www.youtube.com":
+        const paramData = linkData[3].split("=");
+        youtubeId = paramData[1].split("&", 1)[0];
+        break;
+      case "youtu.be":
+        youtubeId = linkData[3];
+        break;
+      default:
         return message.reply(messages);
-      }
-      message.channel.send(
-        "Mohon tunggu, sedang menyiapkan data untuk dikirimkan"
-      );
-      const timeFormat = "Do MMMM YYYY, HH:mm";
-      const dateSplit = args[1].split("/");
-      const date =
-        dateSplit[1] + "/" + dateSplit[0] + "/" + moment().format("YYYY");
-      const dateTime = Date.parse(`${date} ${args[2]}`);
-      const livestreamDateTime = moment(dateTime)
-        .utcOffset("+07:00")
-        .format(timeFormat);
-      const livestreamDateTimeJapan = moment(dateTime)
-        .utcOffset("+09:00")
-        .format(timeFormat);
-      const vliverFirstName = args[0].toLowerCase();
-      const linkData = args[3].split("/");
-      let youtubeId;
-      if (linkData[0] !== "https:" || linkData[3] === "") {
-        return message.reply(messages);
-      }
-      switch (linkData[2]) {
-        case "www.youtube.com":
-          const paramData = linkData[3].split("=");
-          youtubeId = paramData[1].split("&", 1)[0];
-          break;
-        case "youtu.be":
-          youtubeId = linkData[3];
-          break;
-        default:
-          return message.reply(messages);
-      }
-      if (youtubeId === undefined) {
-        return message.reply(messages);
-      }
-      try {
-        const vData = await Vliver.findOne({
-          where: { name: vliverFirstName }
-        });
-        if (!vData) {
-          throw {
-            message: `Kamu menginput ${vliverFirstName} dan itu tidak ada di database kami`
-          };
-        }
-        const youtubeData = await fetchYoutube(youtubeId);
-        const liveEmbed = {
-          color: parseInt(vData.dataValues.color),
-          title: `${vData.dataValues.fullName} akan melakukan Livestream!`,
-          author: {
-            name: vData.dataValues.fullName,
-            icon_url: vData.dataValues.avatarURL,
-            url: vData.dataValues.channelURL
-          },
-          thumbnail: {
-            url: vData.dataValues.avatarURL
-          },
-          fields: [
-            {
-              name: "Tanggal & Waktu Livestream",
-              value: `${livestreamDateTime} GMT+7 / WIB \n${livestreamDateTimeJapan} GMT+9 / JST`
-            },
-            {
-              name: "Link Video Youtube",
-              value: youtubeData.url
-            },
-            {
-              name: "Judul Livestream",
-              value: youtubeData.title
-            }
-          ],
-          image: {
-            url: youtubeData.thumbnailUrl
-          },
-          footer: {
-            text: `${name} v${version} - This message was created on ${moment()
-              .utcOffset("+07:00")
-              .format(timeFormat)}`
-          }
+    }
+    if (youtubeId === undefined) {
+      return message.reply(messages);
+    }
+    try {
+      const vData = await Vliver.findOne({
+        where: { name: vliverFirstName }
+      });
+      if (!vData) {
+        throw {
+          message: `Kamu menginput ${vliverFirstName} dan itu tidak ada di database kami`
         };
-        let mention = "";
-        if (vData.dataValues.fanName || vData.dataValues.fanName === "") {
-          const roleId = message.guild.roles.find(
-            r => r.name === vData.dataValues.fanName
-          );
-          if (roleId) {
-            mention = `<@&${roleId.id}>`;
-          } else {
-            mention = "@here";
+      }
+      const youtubeData = await fetchYoutube(youtubeId);
+      const liveEmbed = {
+        color: parseInt(vData.dataValues.color),
+        title: `${vData.dataValues.fullName} akan melakukan Livestream!`,
+        author: {
+          name: vData.dataValues.fullName,
+          icon_url: vData.dataValues.avatarURL,
+          url: vData.dataValues.channelURL
+        },
+        thumbnail: {
+          url: vData.dataValues.avatarURL
+        },
+        fields: [
+          {
+            name: "Tanggal & Waktu Livestream",
+            value: `${livestreamDateTime} GMT+7 / WIB \n${livestreamDateTimeJapan} GMT+9 / JST`
+          },
+          {
+            name: "Link Video Youtube",
+            value: youtubeData.url
+          },
+          {
+            name: "Judul Livestream",
+            value: youtubeData.title
           }
+        ],
+        image: {
+          url: youtubeData.thumbnailUrl
+        },
+        footer: {
+          text: `${name} v${version} - This message was created on ${moment()
+            .utcOffset("+07:00")
+            .format(timeFormat)}`
+        }
+      };
+      let mention = "";
+      if (vData.dataValues.fanName || vData.dataValues.fanName === "") {
+        const roleId = message.guild.roles.find(
+          r => r.name === vData.dataValues.fanName
+        );
+        if (roleId) {
+          mention = `<@&${roleId.id}>`;
         } else {
           mention = "@here";
         }
-        const channel = message.guild.channels.get(textChannelID.live);
-        await channel.send(
-          `${mention}\n**${vData.dataValues.fullName}** akan melakukan Livestream pada **${livestreamDateTime} WIB!**`,
-          { embed: liveEmbed }
-        );
-        await Schedule.create({
-          title: youtubeData.title,
-          youtubeUrl: youtubeData.url,
-          dateTime: new Date(dateTime),
-          vliverID: vData.dataValues.id,
-          type: "live"
-        });
-        return await message.reply(
-          `Informasi live sudah dikirim ke text channel tujuan.\nNama VLiver: ${vData.dataValues.fullName}\nJudul Livestream: ${youtubeData.title}\nJadwal live: ${livestreamDateTime} WIB / GMT+7`
-        );
-      } catch (err) {
-        message.reply(
-          `Ada sesuatu yang salah, tapi itu bukan kamu: ${err.message}`
-        );
+      } else {
+        mention = "@here";
       }
-    } else {
-      message.reply("", { file: "https://i.imgur.com/4YNSGmG.jpg" });
+      const channel = message.guild.channels.get(textChannelID.live);
+      await channel.send(
+        `${mention}\n**${vData.dataValues.fullName}** akan melakukan Livestream pada **${livestreamDateTime} WIB!**`,
+        { embed: liveEmbed }
+      );
+      await Schedule.create({
+        title: youtubeData.title,
+        youtubeUrl: youtubeData.url,
+        dateTime: new Date(dateTime),
+        vliverID: vData.dataValues.id,
+        type: "live"
+      });
+      return await message.reply(
+        `Informasi live sudah dikirim ke text channel tujuan.\nNama VLiver: ${vData.dataValues.fullName}\nJudul Livestream: ${youtubeData.title}\nJadwal live: ${livestreamDateTime} WIB / GMT+7`
+      );
+    } catch (err) {
+      message.reply(
+        `Ada sesuatu yang salah, tapi itu bukan kamu: ${err.message}`
+      );
     }
   }
 };
--- commands/nh.js
+++ commands/nh.js
@@ -61,8 +61,102 @@ module.exports = {
 
     const timeFormat = "Do MMMM YYYY, HH:mm";
 
-    if (message.channel.id === textChannelID.nh) {
-      if (args.length === 0) {
+    if (!message.member.roles.some(r => roles.nh.includes(r.name))) {
+      return await message.reply("", {
+        file: "https://i.imgur.com/4YNSGmG.jpg"
+      });
+    }
+    if (message.channel.id !== textChannelID.nh) {
+      await message.channel.bulkDelete(1);
+      return await message.author.send(
+        "Jangan gunakan tag itu di sembarang tempat! Ku geplak kamu...",
+        { file: "https://i.imgur.com/FxfX5wL.png" }
+      );
+    }
+    if (args.length === 0) {
+      return message.reply(
+        "Kamu perlu menulis perintah setelah tanda `" +
+          prefix +
+          "nh`. Lihat `" +
+          prefix +
+          "nh help` untuk pilihan perintah"
+      );
+    }
+    switch (args[0]) {
+      case "help":
+        return message.channel.send(
+          "```help: Here's the help then\ninfo <ID>: Fetching the info of the doujin ID\nrandom <number>: Sent you a random doujin\nsearch <keyword>: Finding your doujin based on your keyword\n\nPastikan kamu menggunakan bot ini di Channel Degen. Nanti Cia marah lho```"
+        );
+      case "info":
+        if (!args[1])
+          return message.reply("Kamu perlu menambahkan 6-digit nuklir.");
+        await message.channel.send(
+          `Mencari data untuk doujin ini... ID: ${args[1]}`
+        );
+        return await getDoujin(args[1], message);
+      case "random":
+        const timesRoll = parseInt(args[1]) || 1;
+        const maxRoll = 3;
+        if (timesRoll > maxRoll)
+          return message.reply(
+            `Maksimal yang bisa diberikan ${maxRoll} kali ya...`
+          );
+        await message.channel.send(
+          `Mencarikan${
+            timesRoll > 1 ? " " + timesRoll : ""
+          } doujin favorit yang pas untukmu`
+        );
+        for (let i = 0; i < timesRoll; ++i) {
+          const min = 150000;
+          const max = 301000;
+          const rand = min + Math.random() * (max - min);
+          const number = Math.floor(rand);
+          await getDoujin(number, message);
+        }
+        return await message.channel.send("Semoga kamu suka ya~");
+      case "search":
+        const query = args.slice(1, args.length).join(" ");
+        try {
+          const { data } = await axios.get(
+            `https://nhtai-api.glitch.me/api/search?query=${query}&page=1`
+          );
+          const queryEmbed = {
+            color: 0xec2955,
+            author: {
+              name: "nHentai Fetcher by BotCIA",
+              icon_url:
+                "https://pbs.twimg.com/profile_images/733172726731415552/8P68F-_I_400x400.jpg"
+            },
+            title: `Results of ${query}`,
+            description:
+              data.result.length !== 0
+                ? data.result
+                    .slice(0, 10)
+                    .map(
+                      (dj, i) =>
+                        `${i + 1}. ${dj.title.english}\nhttps://nhentai.net/g/${
+                          dj.id
+                        } | https://nhent.ai/g/${dj.id}`
+                    )
+                    .join("\n\n")
+                : "*No Doujins Found*",
+            fields: [
+              {
+                name: "More Results",
+                value: `https://nhent.ai/search/${query.split(" ").join("%20")}`
+              }
+            ],
+            footer: {
+              text: `${name} v${version} - This message was created on ${moment()
+                .utcOffset("+07:00")
+                .format(timeFormat)}`
+            }
+          };
+          return message.channel.send({ embed: queryEmbed });
+        } catch (err) {
+          console.log(err.message);
+        }
+      default:
         return message.reply(
           "Kamu perlu menulis perintah setelah tanda `" +
             prefix +
@@ -70,109 +164,6 @@ module.exports = {
             prefix +
             "nh help` untuk pilihan perintah"
         );
-      }
-      if (
-        message.member.roles.some(r => roles.nh.includes(r.name)) ||
-        args[0] === "help"
-      ) {
-        switch (args[0]) {
-          case "help":
-            return message.channel.send(
-              "```help: Here's the help then\ninfo <ID>: Fetching the info of the doujin ID\nrandom <number>: Sent you a random doujin\nsearch <keyword>: Finding your doujin based on your keyword\n\nPastikan kamu menggunakan bot ini di Channel Degen. Nanti Cia marah lho```"
-            );
-          case "info":
-            if (!args[1])
-              return message.reply("Kamu perlu menambahkan 6-digit nuklir.");
-            await message.channel.send(
-              `Mencari data untuk doujin ini... ID: ${args[1]}`
-            );
-            return await getDoujin(args[1], message);
-          case "random":
-            const timesRoll = parseInt(args[1]) || 1;
-            const maxRoll = 3;
-            if (timesRoll > maxRoll)
-              return message.reply(
-                `Maksimal yang bisa diberikan ${maxRoll} kali ya...`
-              );
-            await message.channel.send(
-              `Mencarikan${
-                timesRoll > 1 ? " " + timesRoll : ""
-              } doujin favorit yang pas untukmu`
-            );
-            for (let i = 0; i < timesRoll; ++i) {
-              const min = 150000;
-              const max = 301000;
-              const rand = min + Math.random() * (max - min);
-              const number = Math.floor(rand);
-              await getDoujin(number, message);
-            }
-            return await message.channel.send("Semoga kamu suka ya~");
-          case "search":
-            const query = args.slice(1, args.length).join(" ");
-            try {
-              const { data } = await axios.get(
-                `https://nhtai-api.glitch.me/api/search?query=${query}&page=1`
-              );
-              const queryEmbed = {
-                color: 0xec2955,
-                author: {
-                  name: "nHentai Fetcher by BotCIA",
-                  icon_url:
-                    "https://pbs.twimg.com/profile_images/733172726731415552/8P68F-_I_400x400.jpg"
-                },
-                title: `Results of ${query}`,
-                description:
-                  data.result.length !== 0
-                    ? data.result
-                        .slice(0, 10)
-                        .map(
-                          (dj, i) =>
-                            `${i + 1}. ${
-                              dj.title.english
-                            }\nhttps://nhentai.net/g/${
-                              dj.id
-                            } | https://nhent.ai/g/${dj.id}`
-                        )
-                        .join("\n\n")
-                    : "*No Doujins Found*",
-                fields: [
-                  {
-                    name: "More Results",
-                    value: `https://nhent.ai/search/${query
-                      .split(" ")
-                      .join("%20")}`
-                  }
-                ],
-                footer: {
-                  text: `${name} v${version} - This message was created on ${moment()
-                    .utcOffset("+07:00")
-                    .format(timeFormat)}`
-                }
-              };
-              return message.channel.send({ embed: queryEmbed });
-            } catch (err) {
-              console.log(err.message);
-            }
-          default:
-            return message.reply(
-              "Kamu perlu menulis perintah setelah tanda `" +
-                prefix +
-                "nh`. Lihat `" +
-                prefix +
-                "nh help` untuk pilihan perintah"
-            );
-        }
-      } else {
-        return await message.reply("", {
-          file: "https://i.imgur.com/4YNSGmG.jpg"
-        });
-      }
-    } else {
-      await message.channel.bulkDelete(1);
-      return await message.author.send(
-        "Jangan gunakan tag itu di sembarang tempat! Ku geplak kamu...",
-        { file: "https://i.imgur.com/FxfX5wL.png" }
-      );
     }
   }
 };
