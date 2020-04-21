84ce41b93e94d2d2c99501d6d730dce5fcfdc454
Ryuujo <mirukusheki@gmail.com>
2020-04-21 14:31:36 +0700

    gws fix

--- commands/gws.js
+++ commands/gws.js
@@ -3,18 +3,10 @@ const config = require("../config.js");
 module.exports = {
   name: "gws",
   description: "Says Get well soon to someone",
-  execute(message) {
-    const args = message.content.slice(config.prefix.length).split(/ +/);
-    if (!message.mentions.users.size && args[1]) {
-      if (args[1][0] !== "@") {
-        return message.reply("Kamu harus mention user tersebut!");
-      }
-    }
-    if (message.mentions.users.size > 0) {
+  execute(message, args) {
+    if (args.length > 0) {
       message.channel.send(
-        `GWS untuk kamu ${message.mentions.users
-          .map(user => `<@${user.id}>`)
-          .join(", ")}`
+        `GWS untuk kamu, ${args[0]}`
       );
       return;
     }
