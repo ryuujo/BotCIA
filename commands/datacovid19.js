6922ebff0a682a978a036064bded43d258c938a2
Ryuujo <mirukusheki@gmail.com>
2020-06-29 12:51:57 +0700

    kawal covid patch, tag rank added

--- commands/datacovid19.js
+++ commands/datacovid19.js
@@ -9,7 +9,7 @@ module.exports = {
     const timeFormat = 'Do MMMM YYYY, HH:mm Z';
     try {
       const dataIndonesia = await axios.get(
-        'https://api.kawalcovid19.id/v1/api/case/summary'
+        'https://api.kawalcorona.com/indonesia'
       );
       const positifGlobal = await axios.get(
         'https://api.kawalcorona.com/positif'
@@ -28,52 +28,43 @@ module.exports = {
           'Menampilkan informasi status COVID-19 di Indonesia dan Dunia.',
         author: {
           name: 'Kawal COVID-19 #StayAtHome',
-          url: 'https://kawalcorona.com/',
-        },
-        thumbnail: {
-          url: 'https://kawalcovid19.id/android-chrome-192x192.png',
+          url: 'https://kawalcorona.com/'
         },
         fields: [
           {
             name: 'Jumlah positif di Indonesia',
-            value: dataIndonesia.data.confirmed.toLocaleString(),
+            value: dataIndonesia.data[0].positif.toLocaleString()
           },
           {
             name: 'Dalam Perawatan',
-            value: dataIndonesia.data.activeCare.toLocaleString(),
-            inline: true,
+            value: dataIndonesia.data[0].dirawat.toLocaleString(),
+            inline: true
           },
           {
             name: 'Sembuh',
-            value: dataIndonesia.data.recovered.toLocaleString(),
-            inline: true,
+            value: dataIndonesia.data[0].sembuh.toLocaleString(),
+            inline: true
           },
           {
             name: 'Meninggal',
-            value: dataIndonesia.data.deceased.toLocaleString(),
-            inline: true,
+            value: dataIndonesia.data[0].meninggal.toLocaleString(),
+            inline: true
           },
           positifGlobal.data,
           sembuhGlobal.data,
-          meninggalGlobal.data,
-          {
-            name: 'Laporan Update Terakhir',
-            value: moment(dataIndonesia.data.metadata.lastUpdatedAt)
-              .utcOffset('+07:00')
-              .format(timeFormat),
-          },
+          meninggalGlobal.data
         ],
         footer: {
-          text: 'Data diambil melalui website https://kawalcovid19.id',
-        },
+          text: 'Data diambil melalui website https://kawalcorona.com'
+        }
       };
       return await message.channel.send(
         'Berikut adalah hasil terakhir laporan COVID-19',
         { embed: generalEmbed }
       );
     } catch (err) {
-      message.channel.send('Ada sesuatu yang salah, tapi itu bukan kamu')
+      message.channel.send('Ada sesuatu yang salah, tapi itu bukan kamu');
       console.log(err);
     }
-  },
+  }
 };
